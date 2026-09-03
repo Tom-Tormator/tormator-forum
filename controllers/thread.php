acdb3284ab6aa69ca7fca9ccc551c0591abcee7d
Tom Tormator <tormanny@proton.me>
2026-09-03 14:29:47 -0700

    Add header to every file as per the license

--- controllers/thread.php
+++ controllers/thread.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 // thread.php
 // Shows the posts in a thread and allows users to post.
 
